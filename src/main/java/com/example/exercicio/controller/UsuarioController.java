f9de8dc3c5e440f8cc9b1660199b06005157f331
Moizés Júnior <moizes.tj@gmail.com>
2023-04-17 00:06:15 -0300

    remoção do @service

--- src/main/java/com/example/exercicio/controller/UsuarioController.java
+++ src/main/java/com/example/exercicio/controller/UsuarioController.java
@@ -3,7 +3,6 @@ package com.example.exercicio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import com.example.exercicio.model.Usuario;
 import com.example.repository.UsuarioRepository;
 
 @RestController
-@Service
 public class UsuarioController {
 
   @PostMapping(value = "/usuarios")
